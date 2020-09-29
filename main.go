4d3faa5b00ffcafd431b1d7ac5ecdd4e2d932bac
Jonathan Díaz <jonathan.dguevara@alumnos.udg.mx>
2020-09-29 11:47:19 -0500

    Area del triangulo

--- main.go
+++ main.go
@@ -2,6 +2,12 @@ package main
 
 import "fmt"
 
-func main()  {
-	fmt.Println("Hola Mundo!") // eliminar esta línea
+func main(){
+	var base float64
+	var altura float64
+	var area float64
+	fmt.Scanln(&base)
+	fmt.Scanln(&altura)
+	area = (base * altura)/2
+	fmt.Println(area)
 }
